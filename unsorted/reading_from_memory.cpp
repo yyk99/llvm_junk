06eb2a0c1afa1edb9526d20bcae66e3abc572cf6
yyk <yyk@alpenglow>
2020-08-22 16:49:33 -0500

    Updated unsorted dir

---
+++ unsorted/reading_from_memory.cpp
@@ -0,0 +1,91 @@
+//
+//
+//
+
+// #define NDEBUG /* disable accerts */
+
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h"
+
+#include <vector>
+#include <iostream>
+
+using namespace llvm;
+
+static LLVMContext Context;
+static Module *ModuleOb = new Module("my compiler", Context);
+static std::vector<std::string> FunArgs;
+
+Function *createFunc(IRBuilder<> &Builder, std::string Name)
+{
+    Type *u32Ty = Type::getInt32Ty(Context);
+    Type *vecTy = VectorType::get(u32Ty, 2);
+    Type *ptrTy = vecTy->getPointerTo(0);
+    FunctionType *funcType =
+        FunctionType::get(Builder.getInt32Ty(), ptrTy, false);
+    Function *fooFunc =
+        Function::Create(funcType, Function::ExternalLinkage, Name, ModuleOb);
+    return fooFunc;
+}
+
+void setFuncArgs(Function *fooFunc, std::vector<std::string> FunArgs)
+{
+    unsigned Idx = 0;
+
+    Function::arg_iterator AI, AE;
+    for (AI = fooFunc->arg_begin(), AE = fooFunc->arg_end(); AI != AE;
+         ++AI, ++Idx)
+        AI->setName(FunArgs[Idx]);
+}
+
+BasicBlock *createBB(Function *fooFunc, std::string Name)
+{
+    return BasicBlock::Create(Context, Name, fooFunc);
+}
+
+Value *getGEP(IRBuilder<> &Builder, Value *Base, Value *Offset)
+{
+    {
+        auto t1 = cast<PointerType>(Base->getType()->getScalarType())->getElementType();
+        std::cout << t1 << std::endl;
+        t1->print(llvm::errs());
+        llvm::errs() << "\n";
+    }
+    return Builder.CreateGEP(Builder.getInt32Ty(), Base, Offset, "a1");
+}
+
+Value *getLoad(IRBuilder<> &Builder, Value *Address)
+{
+    return Builder.CreateLoad(Address, "load");
+}
+
+int main(int argc, char *argv[])
+{
+    FunArgs.push_back("a");
+    static IRBuilder<> Builder(Context);
+    Function *fooFunc = createFunc(Builder, "foo");
+    setFuncArgs(fooFunc, FunArgs);
+    Value *Base = fooFunc->arg_begin();
+    BasicBlock *entry = createBB(fooFunc, "entry");
+    Builder.SetInsertPoint(entry);
+    Value *gep = getGEP(Builder, Base, Builder.getInt32(1));
+
+    Value *load = getLoad(Builder, gep);
+    Builder.CreateRet(load);
+    verifyFunction(*fooFunc);
+    ModuleOb->dump();
+    return 0;
+}
+
+// Sarda, Suyog. LLVM Essentials: Become familiar with the LLVM infrastructure and start using LLVM libraries to design a compiler (pp. 42-43). Packt Publishing. Kindle Edition. 
+
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// tab-width: 4
+// indent-tabs-mode: nil
+// End:
+
