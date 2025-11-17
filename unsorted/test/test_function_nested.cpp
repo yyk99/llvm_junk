697326d265f3b3544ee2dec80224340fb7f7c00f
Yura K <yyk@silvana.home>
2025-11-16 20:52:36 -0500

    Update test/test_function_nested.cpp

--- unsorted/test/test_function_nested.cpp
+++ unsorted/test/test_function_nested.cpp
@@ -20,16 +20,25 @@ using namespace llvm;
 
 static LLVMContext Context;
 
-class test_function_with_args : public test_llvm_basic {
+class test_function_nested : public test_llvm_basic {
 public:
 
     Function *createFunc(IRBuilder<> &Builder, std::string Name, Module *ModuleOb,
                         std::vector<std::string> const &FunArgs)
     {
         std::vector<Type *> Integers(FunArgs.size(), Type::getInt32Ty(Context));
-        FunctionType *funcType = llvm::FunctionType::get(Builder.getInt32Ty(), Integers, false);
-        Function *fooFunc =
-            llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, Name, ModuleOb);
+        auto funcType = FunctionType::get(Builder.getInt32Ty(), Integers, false);
+        auto fooFunc = Function::Create(funcType, Function::ExternalLinkage, Name, ModuleOb);
+
+        return fooFunc;
+    }
+
+    Function *createInternalFunc(IRBuilder<> &Builder, std::string Name, Module *ModuleOb,
+                        std::vector<std::string> const &FunArgs)
+    {
+        std::vector<Type *> Integers(FunArgs.size(), Type::getInt32Ty(Context));
+        auto funcType = FunctionType::get(Builder.getInt32Ty(), Integers, false);
+        auto fooFunc = Function::Create(funcType, Function::InternalLinkage, Name, ModuleOb);
 
         return fooFunc;
     }
@@ -58,26 +67,67 @@ public:
     }
 };
 
-//  Sarda, Suyog. LLVM Essentials: Become familiar with the LLVM infrastructure and start using LLVM
-//  libraries to design a compiler (pp. 20-21). Packt Publishing. Kindle Edition.
+/*
+//
+// https://mapping-high-level-constructs-to-llvm-ir.readthedocs.io/en/latest/advanced-constructs/lambda-functions.html
+//
 
-TEST_F(test_function_with_args, main)
+int foo(int a)
 {
-    std::vector<std::string> FunArgs;
-    FunArgs.push_back("a");
-    FunArgs.push_back("b");
+  auto function = [a](int x) { return x + a; };
+  return function(10);
+}
 
+define internal i32 @lambda(i32 %a, i32 %x) {
+	%1 = add i32 %a, %x
+	ret i32 %1
+}
+
+define i32 @foo(i32 %a) {
+	%1 = call i32 @lambda(i32 %a, i32 10)
+	ret i32 %1
+}
+
+*/
+
+TEST_F(test_function_nested, main)
+{
     IRBuilder<> Builder(Context);
     auto ModuleOb = std::make_unique<Module>("my compiler", Context);
 
-    GlobalVariable *gVar = createGlob(Builder, "x", ModuleOb.get());
-    Function *fooFunc = createFunc(Builder, "foo", ModuleOb.get(), FunArgs);
-    setFuncArgs(fooFunc, FunArgs);
-    BasicBlock *entry = createBB(fooFunc, "entry");
-    Builder.SetInsertPoint(entry);
-    Builder.CreateRet(Builder.getInt32(0));
-    verifyFunction(*fooFunc);
+    auto lambda = createInternalFunc(Builder, "lambda", ModuleOb.get(), {"a", "x"});
 
+    auto fooFunc = createFunc(Builder, "foo", ModuleOb.get(), {"a"});
+    {
+        setFuncArgs(fooFunc, {"a"});
+        BasicBlock *entry = createBB(fooFunc, "entry");
+        Builder.SetInsertPoint(entry);
+
+        auto args = lambda->arg_begin();
+        Value *a = args++;
+        Value *x = Builder.getInt32(10); // Second parameter: 10
+
+        // Call the function using CreateCall
+        Value *callResult = Builder.CreateCall(lambda, {a, x});
+
+        Builder.CreateRet(callResult);
+        verifyFunction(*fooFunc);
+    }
+    {
+        setFuncArgs(lambda, {"a", "x"});
+        BasicBlock *entry = createBB(lambda, "entry");
+        Builder.SetInsertPoint(entry);
+        // Name the parameters
+        auto args = lambda->arg_begin();
+        Value *a = args++;
+        a->setName("a");
+        Value *x = args++;
+        x->setName("x");
+
+        Value *result = Builder.CreateAdd(a, x);
+        Builder.CreateRet(result);
+        verifyFunction(*lambda);
+    }
     // Create workspace directory for output
     auto ws = create_workspace();
     auto output_file = ws / "output.ll";
@@ -87,24 +137,14 @@ TEST_F(test_function_with_args, main)
     ASSERT_FALSE(EC) << "Failed to open output file: " << EC.message();
 
     ModuleOb->print(output, nullptr);
-    output.close();
+#ifndef NDEBUG
+    ModuleOb->dump();
+#endif
 
     // Verify file was created and has content
     ASSERT_TRUE(fs::is_regular_file(output_file)) << "Output file was not created";
     EXPECT_LE(0, fs::file_size(output_file)) << "Output file is empty";
-
-    // Verify function has correct number of arguments
-    EXPECT_EQ(fooFunc->arg_size(), 2) << "Function should have 2 arguments";
-
-    // Verify argument names
-    auto arg_it = fooFunc->arg_begin();
-    EXPECT_EQ(arg_it->getName(), "a") << "First argument should be named 'a'";
-    ++arg_it;
-    EXPECT_EQ(arg_it->getName(), "b") << "Second argument should be named 'b'";
 }
-
-// Sarda, Suyog. LLVM Essentials: Become familiar with the LLVM infrastructure and start using LLVM
-// libraries to design a compiler (pp. 21-22). Packt Publishing. Kindle Edition.
 
 // Local Variables:
 // mode: c++
