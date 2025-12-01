36741df0fb7fc87a9d68f34f190d632844d53485
Yury Kuznetsov <yyk99@kestrel.home>
2025-12-01 08:11:34 -0500

    WIP
    Fixed a null reference in verbose mode

--- mini/compiler/test/test_t2.cpp
+++ mini/compiler/test/test_t2.cpp
@@ -673,6 +673,28 @@ end program exit_standard;
 )", true}
   )
 );
+
+INSTANTIATE_TEST_SUITE_P
+(
+ ErrorReporting,
+ CompilerP,
+ ::testing::Values
+ (
+  CP {
+      R"(/* syntax error 1 */
+program syntax_error_1:
+    declare a array [10] of integer;
+    declare i integer;
+
+    for i := 1 to 10
+    do
+        /* set */ a[i] := i;
+    end for i;
+end program syntax_error_1;
+)", true, 1, 1}
+  )
+);
+
 // clang-format on
 
 // Local Variables:
